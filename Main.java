4ffb3519d27fe5df1c5a9fed7e4c011a964e4e5d
Vinayak patil <vinayakpatil2103@gmail.com>
2026-02-03 16:08:46 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,45 @@
+package com.vinu;
+import java.sql.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws ClassNotFoundException{
+        String url ="jdbc:mysql://localhost:3306/mydatabase";
+        String username="root";
+        String password = "Vinayak@102711";
+        String query="select * from employees;";
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Drivers loaded successfully");
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            Connection con =DriverManager.getConnection(url, username,password);
+            System.out.println("Connection establish successfully.");
+            Statement stmt=con.createStatement();
+            ResultSet rs=stmt.executeQuery(query);
+            while (rs.next()){
+                int id=rs.getInt("id");
+                String name=rs.getString("name");
+                String job_title=rs.getString("job_title");
+                Double salary =rs.getDouble("salary");
+                System.out.println();
+                System.out.println("==========================");
+                System.out.println("ID : "+id);
+                System.out.println("Name : "+name);
+                System.out.println("job_title : "+job_title);
+                System.out.println("salary : "+salary);
+                }
+                rs.close();
+            stmt.xc();
+            con.close();
+            System.out.println("Connection closed successfully");
+         }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
